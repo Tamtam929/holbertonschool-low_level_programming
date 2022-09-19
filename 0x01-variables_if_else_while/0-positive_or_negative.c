0d789f5595702a2441d6c19dc04d1b157e030f2b
Tamtam929 <tamtam929@gmail.com>
2022-09-19 15:35:51 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 /*
  * File: 1-last_digit.c
  * Auth: Tammy Junk
- * */
+ */
 
 #include <stdlib.h>
 #include <time.h>
@@ -20,19 +20,19 @@ int main(void)
 	srand(time(0));
 	n = rand () - RAND_Max / 2;
 
-	if ((n%10) >5)
+	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
+		printf("Last digit of % d is % d and is greater than 5\n",
 				n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		printf("Last digit of % d is % d and is less than 6 and not 0\n",
 				n, n % 10));
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n",
+		printf("Last digit of  d is % d and is 0\n",
 	}
 
 	return (0);
