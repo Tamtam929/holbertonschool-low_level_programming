844252bc5f3e4ac4399c044f1abbaa5815ea5c41
Tamtam929 <tamtam929@gmail.com>
2022-09-22 09:41:49 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print _alphabet - prints lowercase alphabet
+ * Return : 0
+ * /
+ *
+ * void print_alphabet(void)
+ * {
+ * char alpha;
+ *
+ * alpha = 'a';
+ *
+ * while (alpha <= 'z')
+ * {
+ * _putchar(alpha);
+ * {
+ * _putchar(alpha);
+ * alpha++;
+ * }
+ * _putchar('\n');
