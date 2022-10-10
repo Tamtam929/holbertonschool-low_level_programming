ee55857e624289d838af39eef9283be4df5563eb
Tamtam929 <tamtam929@gmail.com>
2022-10-10 16:17:49 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,51 @@
-#include <stdio.h>
+#includeii <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
- */
+ * num_checker - checks if a given char is number or not
+ * @a: char to be checked
+ * Return: 1, if its a number, else 0
+ **/
+int num_checker(char *a)
+{
+	int i, num, len;
+
+	i = 0;
+	num = 0;
+	len = strlen(a);
+	while (i < len)
+	{
+		if (a[i] < '0' || a[i] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (a[i] - '0');
+		i++;
+	}
+	return (num);
+}
+/**
+ * main - add positive numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of addition or 1
+ **/
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, num, sum;
 
-	if (argc < 1)
-		return (0);
-
+	sum = 0;
 	for (i = 1; i < argc; i++)
+		num = num_checker(argv[i]);
+	if (num == -1)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", sum);
+	sum += num;
 
-	return (0);
+}
+printf("%d\n", sum);
+return (0);
 }
