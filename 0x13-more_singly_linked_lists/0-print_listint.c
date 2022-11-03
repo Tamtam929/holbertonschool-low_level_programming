7ae55f8c8f2cb2b9a6713ea2b9a9e337dcb245fd
Tamtam929 <tamtam929@gmail.com>
2022-11-03 16:04:56 -0700

    moresingly

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes;
-
-	nodes = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		nodes++;
 		h = h->next;
+		++i;
 	}
-	return (nodes);
+	return (i);
 }
