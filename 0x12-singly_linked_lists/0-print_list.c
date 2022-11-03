349be693c1498b60c69eb8538d68c2e76170f7e0
Tamtam929 <tamtam929@gmail.com>
2022-11-03 10:58:21 -0700

    singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,14 +11,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	int count = 0;
 
+	if (h == NULL)
+		return (0);
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
-	return (nodes);
+	return (count);
 }
